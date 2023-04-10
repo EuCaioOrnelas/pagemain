e37a39543bb47d5775da796347c7bf6a306ad9fd
Caio Ornelas <caioornelasoficial@gmail.com>
2023-04-09 21:55:41 -0300

    UseOutsideClick

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,89 @@
 import styles from "./Navbar.module.css";
 
+import { BiMenu } from "react-icons/bi";
+import { useOutsideClick } from "./useOutsideClick";
+
 function Navbar() {
+  const dropDownRef = useRef(null);
+  const [isActive, setIsActive] = useOutsideClick(dropDownRef, false);
+  const onClick = () => setIsActive(isActive);
+
+  console.log(isActive);
+
   return (
     <>
-      <div className="header">
-        <div className={styles.contentHeader}>
-          <div className={styles.headerText}>
-            <h4>O Maior aplicativo de agendamentos do Brasil</h4>
+      <header>
+        <div className="header">
+          <div className={styles.contentHeader}>
+            <div className={styles.headerText}>
+              <h4>O Maior aplicativo de agendamentos do Brasil</h4>
+            </div>
+            <div className={styles.iconNavbar}>
+              <img src="../public/brazil.png" alt="Bandeira do Brasil" />
+            </div>
           </div>
-          <div className={styles.iconNavbar}>
-            <img src="../public/brazil.png" alt="Bandeira do Brasil" />
+          <div className={styles.lineNavbar}></div>
+          <div className={styles.navbar}>
+            <div className={styles.navbarLogo}>
+              <img src="../public/logo.png" />
+              <h2>AgendamentosPop</h2>
+            </div>
+            <div className={styles.navbarLinks}>
+              <ul>
+                <li>
+                  <a href="">Incio</a>
+                </li>
+                <li>
+                  <a href="">Sobre</a>
+                </li>
+                <li>
+                  <a href="">Vantagens</a>
+                </li>
+                <li>
+                  <a href="">Planos</a>
+                </li>
+                <li>
+                  <a href="">Contato</a>
+                </li>
+              </ul>
+              <a href="#" className={styles.action_btn}>
+                Baixe Agora
+              </a>
+            </div>
+            <div className={styles.iconMenu} onClick={onClick}>
+              <BiMenu />
+            </div>
           </div>
+
+          <div className={styles.lineNavbarEnd}></div>
         </div>
-        <div className={styles.lineNavbar}></div>
-        <div className={styles.navbar}>
-          <div className={styles.navbarLogo}>
-            <img src="../public/logo.png" />
-            <h2>AgendamentosPop</h2>
+        <nav ref={dropDownRef}>
+          <div className={styles.NavbarResponsive}>
+            <div className={styles.boxMenuResponsive}>
+              <ul className={styles.navbarLinksResponsive}>
+                <li>
+                  <a href="">Incio</a>
+                </li>
+                <li>
+                  <a href="">Sobre</a>
+                </li>
+                <li>
+                  <a href="">Vantagens</a>
+                </li>
+                <li>
+                  <a href="">Planos</a>
+                </li>
+                <li>
+                  <a href="">Contato</a>
+                </li>
+              </ul>
+              <a href="#" className={styles.action_btn}>
+                Baixe Agora
+              </a>
+            </div>
           </div>
-          <div className={styles.navbarLinks}>
-            <ul>
-              <li>
-                <a href="">Incio</a>
-              </li>
-              <li>
-                <a href="">Sobre</a>
-              </li>
-              <li>
-                <a href="">Vantagens</a>
-              </li>
-              <li>
-                <a href="">Planos</a>
-              </li>
-              <li>
-                <a href="">Contato</a>
-              </li>
-            </ul>
-            <a href="#" className={styles.action_btn}>
-              Baixe Agora
-            </a>
-          </div>
-        </div>
-        <div className={styles.lineNavbarEnd}></div>
-      </div>
+        </nav>
+      </header>
     </>
   );
 }
